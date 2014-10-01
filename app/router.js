407adbad27c1ebd0f6f44776d90e1746d3a6260c
kaluza.simon@gmail.com <kaluza.simon@gmail.com>
2014-10-01 19:11:53 -0400

    Initial modularization of the sample Blog to test ember-cli functionality

--- app/router.js
+++ app/router.js
@@ -6,6 +6,11 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home', {path: '/'});
+  this.route('about');
+  // this.resource('posts', { path: '/blog'}, function(){
+  //   this.route('index');
+  // });
 });
 
 export default Router;
